38f95ad2bcd04897480b0d90f0d169b9699b9572
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-03-17 05:06:08 -0700

    Todo commit

--- App.js
+++ App.js
@@ -1,42 +1,32 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import Header from './Components/Header';
 
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'Nani' , id: '1'},
-    {name: 'Kal' , id: '2'},
-    {name: 'Seni' , id: '3'},
-    {name: 'Amen' , id: '4'},
-    {name: 'Kiru' , id: '5'},
-    {name: 'Almi' , id: '6'},
-    {name: 'Hope' , id: '7'},
-  ]);
 
-  const pressHandler = (id) =>{
-    console.log(id);
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person => person.id != id)
-    });
-  } 
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee' , key: '1'},
+    { text: 'create an app' , key: '2'},
+    { text: 'play on the switch' , key: '3'}
+  ]);
 
   return (
     <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+       {/* Todo form */}
+          <View style={styles.list}>
+            <FlatList 
+              data={ todos }
+              renderItem={( { item } )=>(
+                  <Text>{item.text}</Text>
+              )}
+            />
+        </View>
 
-    <FlatList 
-        numColumns={2}
-        keyExtractor={(item)=>item.id}
-        data={people}
-        renderItem={( {item} )=> (
-          <>
-            <TouchableOpacity onPress={() => pressHandler(item.id)}>
-              <Text style={styles.item}>{item.name}</Text>
-            </TouchableOpacity>
-          </>
-        )}
-    />
-        
+      </View> 
     </View>
   );
 }
@@ -45,18 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20
   }
-  
 });
